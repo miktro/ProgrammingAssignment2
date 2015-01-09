79adc39667e2079107b7fac309de50a76ffd657b
miktro <miktro314@gmail.com>
2015-01-09 15:32:21 +0000

    Completed and tested the 2 functions
    Completed and tested the 2 functions.
    Used system.time to compare speed of solve vs cacheSolve
    on a 10x10 matrix.
    cacheSolve was (only) 12 times faster¬!

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This creates a new object that stores a matrix 
+## together with it's inverse - initially set to NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mi <- NULL
+    set <- function(y) {
+        x <<- y
+        mi <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) mi <<- inv
+    getinverse <- function() mi
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mi <- x$getinverse()
+    if(!is.null(mi)) {
+        #message("getting cached matrix inverse")
+        return(mi)
+    }
+    data <- x$get()
+    mi <- solve(data, ...)
+    x$setinverse(mi)
+    mi
 }
